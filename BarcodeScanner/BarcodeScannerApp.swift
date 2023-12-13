5ee8074411ae357d9b197f4b9206a2f5b57ae3ae
lmsand <lmsand99@icloud.com>
2023-12-13 02:50:44 -0600

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by lexi sanders on 12/13/23.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
